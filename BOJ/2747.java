67f13c5344ff0edab7c597bd20e360dafe4c3942
yerimsw <swyr1016@gmail.com>
2022-09-01 21:31:36 +0900

    BOJ 2747 피보나치 수

---
+++ BOJ/2747.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] fibo = new int[46];
+
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+
+        System.out.println(fibo[n]);
+    }
+}
+// for문을 이용한 피보나치함수 풀이
