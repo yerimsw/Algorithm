073a117c3dda6fc8d8b22f532329d3994f4dac4b
Yerim Seonwoo <54367532+yerimsw@users.noreply.github.com>
2022-12-07 23:46:03 +0900

    Create Lv.0 모음 제거.java

---
+++ Programmers/Lv.0/모음 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u') continue;
+            
+            sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
