dbbe3b4f791f49266b45d4324c5aed2788e7bf35
Yerim Seonwoo <54367532+yerimsw@users.noreply.github.com>
2023-01-07 23:56:37 +0900

    Lv.0 숨어있는 숫자의 덧셈

---
+++ Programmers/Lv.0/숨어있는 숫자의 덧셈.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string) {
+		int answer = 0;
+		String[] str = my_string.split("[a-zA-Z]");
+		for (int i = 0; i < str.length; i++) {
+			if(str[i].isEmpty()) continue;
+      answer += Integer.parseInt(str[i]);
+    }
+		return answer;
+	}
+}
